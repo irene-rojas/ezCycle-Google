5c349b54a56dc4e052a40b69d01856915a28e87a
Irene R <irene@smallwondermedia.com>
2019-07-05 23:58:19 -0400

    upgrade handlebars

---
+++ src/components/Map.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Map = () => {
+
+  return (
+
+    <iframe src="https://www.google.com/maps/d/embed?mid=1saS2CBlexDCqE7fFurEuL2L0tsoRFw_l" width="640" height="480"></iframe>
+  )
+}
+
+
+export default Map;
